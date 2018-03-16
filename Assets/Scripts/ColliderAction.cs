765310e172e7d514909b295969cd10caaa5a53a4
Alexandre Ganito <alexandre.ganito@gmail.com>
2018-03-16 01:22:13 -0400

    Collider for cards appearence

---
+++ Assets/Scripts/ColliderAction.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderAction : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Entered");
+        this.transform.GetChild(0).gameObject.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Exited");
+        this.transform.GetChild(0).gameObject.SetActive(false);
+    }
+}
