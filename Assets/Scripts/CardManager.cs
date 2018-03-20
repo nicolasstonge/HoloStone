0e9d3eea86d30f6a4691a4873914c0f5006fadb9
xyrodileas <xyrodileas@gmail.com>
2018-03-20 10:59:23 -0400

    Add the CardManager and start of logic to keep track of cards on the board

---
+++ Assets/Scripts/CardManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+// CardManager to place on the scene, only one should be present.
+// The cards should be a child GameObjet of the CardManager GameObject
+public class CardManager : MonoBehaviour
+{
+    private List<CardAsset> mlistCards = new List<CardAsset>();
+
+    int interval = 1;
+    float nextTime = 0;
+
+    // When a new card pop on the board, this function is called
+    public void OnTrackedCard(CardAsset card)
+    {
+        mlistCards.Add(card);
+    }
+
+    // When a new card is removed from the board, this function is called
+    public void LostTrackedCard(CardAsset card)
+    {
+        mlistCards.Remove(card);
+    }
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Print the list of cards every secondes (For Debug purpose)
+        if (Time.time >= nextTime)
+        {
+            Debug.Log("List of currently In-Game cards :");
+            foreach (CardAsset cardInGame in mlistCards)
+            {
+                Debug.Log(cardInGame.Description);
+            }
+            nextTime += interval;
+        }
+        
+
+    }
+}
---
+++ Assets/Scripts/DetectorAction.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class DetectorAction : MonoBehaviour, ITrackableEventHandler
+{
+    private TrackableBehaviour mTrackableBehaviour;
+    // Ref on the CardManager
+    private CardManager mCardManager;
+    // Ref on the CardAsset 
+    private CardAsset mCardAsset;
+
+    // Function triggered when the object tracking state change (AKA detected or not detected)
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+                newStatus == TrackableBehaviour.Status.TRACKED ||
+                newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            
+            if(!mCardAsset)
+            {
+                mCardAsset = GetComponent<CardAsset>();
+            }
+            if(!mCardManager)
+            {
+                mCardManager = this.transform.parent.GetComponent<CardManager>();
+            }
+            // Notify the CardManager
+            mCardManager.OnTrackedCard(mCardAsset);
+
+        }
+        else
+        {
+            // mCardAsset is not available at Start(), if null, set it up
+            if (!mCardAsset)
+            {
+                mCardAsset = GetComponent<CardAsset>();
+            }
+            // mCardManager is not available at Start(), if null, set it up
+            if (!mCardManager)
+            {
+                mCardManager = this.transform.parent.GetComponent<CardManager>();
+            }
+            // Notify the CardManager
+            mCardManager.LostTrackedCard(mCardAsset);
+        }
+    }
+
+
+    void Start () {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
