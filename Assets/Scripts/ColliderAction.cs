d6a81599703517d7a236ac05d73bf6e95551cfec
Alexandre Ganito <alexandre.ganito@gmail.com>
2018-03-20 19:09:57 -0400

    Added Board and monster placement on it

--- Assets/Scripts/ColliderAction.cs
+++ Assets/Scripts/ColliderAction.cs
@@ -8,12 +8,16 @@ public class ColliderAction : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Entered");
-        this.transform.GetChild(0).gameObject.SetActive(true);
+
+        GameObject card = this.transform.GetChild(0).gameObject;
+        card.transform.position = other.transform.position;
+        card.transform.rotation = other.transform.rotation;
+        card.transform.parent = other.transform.parent.transform;
+        card.SetActive(true);
     }
 
     private void OnTriggerExit(Collider other)
     {
         Debug.Log("Exited");
-        this.transform.GetChild(0).gameObject.SetActive(false);
     }
 }
