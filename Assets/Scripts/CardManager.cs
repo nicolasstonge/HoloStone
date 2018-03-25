5fed738752a55c0368d426498f9652b3327b9f25
xyrodileas <xyrodileas@gmail.com>
2018-03-25 16:33:38 -0400

    Add logic for game management in the GameManager

--- Assets/Scripts/CardManager.cs
+++ Assets/Scripts/CardManager.cs
@@ -9,8 +9,20 @@ public class CardManager : MonoBehaviour
 {
     private List<CardAsset> mlistCards = new List<CardAsset>();
 
-    int interval = 1;
-    float nextTime = 0;
+    int iInterval = 1;
+    float fNextTime = 0;
+    bool bTurnPlayer = true;
+    bool bGameStarted = false;
+    List<CardAsset> lCardsPlayer;
+    List<CardAsset> lCardsAI;
+    int iPlayerLife = -1;
+    int iAILife = -1;
+
+    int iCurrentPlayerActionPoints = -1;
+
+    const int STARTING_LIFE = 5;
+    const int ACTION_POINT = 3;
+
 
     // When a new card pop on the board, this function is called
     public void OnTrackedCard(CardAsset card)
@@ -29,18 +41,40 @@ public class CardManager : MonoBehaviour
     void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    public void StartGame()
+    {
+        lCardsAI = new List<CardAsset>();
+        lCardsPlayer = new List<CardAsset>();
+        iPlayerLife = STARTING_LIFE;
+        iAILife = STARTING_LIFE;
+
+        bGameStarted = true;
+    }
+
+    public void PassTurnPlayer()
+    {
+        bTurnPlayer = !bTurnPlayer;
+        iCurrentPlayerActionPoints = ACTION_POINT;
+
+
+        if (!bTurnPlayer)
+        {
+            // Call AI to play a turn?
+        }
+
+    }
+    // Update is called once per frame
+    void Update () {
         // Print the list of cards every secondes (For Debug purpose)
-        if (Time.time >= nextTime)
+        if (Time.time >= fNextTime)
         {
             Debug.Log("List of currently In-Game cards :");
             foreach (CardAsset cardInGame in mlistCards)
             {
                 Debug.Log(cardInGame.Description);
             }
-            nextTime += interval;
+            fNextTime += iInterval;
         }
         
 
