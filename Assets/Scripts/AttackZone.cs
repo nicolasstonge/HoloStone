00075c610a1ba769aeba5a8ed0e4fd5fdb221077
Nicolas St-Onge <stongenicolas@hotmail.com>
2018-03-27 18:17:33 -0400

    add new scene and script for attacks

---
+++ Assets/Scripts/AttackZone.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackZone : MonoBehaviour {
+
+    GameObject parent;
+
+	// Use this for initialization
+	void Start () {
+
+        parent = transform.parent.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.gameObject == parent.GetComponent<MonsterAnim>().targetmonster)
+        {
+
+        }
+    }
+}
