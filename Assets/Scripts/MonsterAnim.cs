d1f68388e42b862e0c29ef6f4014c447de4a5250
ILE\udizgal <alexandre.diz.ganito@ville.montreal.qc.ca>
2018-04-16 11:04:14 -0400

    Try to reset to version 2.0

--- Assets/Scripts/MonsterAnim.cs
+++ Assets/Scripts/MonsterAnim.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using cakeslice;
+using HoloToolkit.Unity.InputModule;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class MonsterAnim : MonoBehaviour {
+public class MonsterAnim : MonoBehaviour, IInputClickHandler
+{
 
     NavMeshAgent nav;
     Animator animator;
@@ -15,21 +18,26 @@ public class MonsterAnim : MonoBehaviour {
     Boolean attacking;
     Boolean moving;
     Boolean rotate;
+    public Outline outliner;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
 
-        
+
         nav = transform.GetComponent<NavMeshAgent>();
         animator = transform.GetComponent<Animator>();
         combatManager = GameObject.Find("Board").GetComponent<CombatManager>();
         attacking = false;
         moving = false;
         rotate = false;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        outliner = gameObject.GetComponentInChildren<Outline>();
+        outliner.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
 
 
         if (!nav.pathPending && moving)
@@ -48,13 +56,15 @@ public class MonsterAnim : MonoBehaviour {
                     }
                     else
                     {
-                        
+
                         nav.stoppingDistance = 0.08f;
                         moving = false;
                         rotate = true;
                         animator.SetTrigger("idle");
+                        disableOutline();
+                        targetmonster.GetComponent<MonsterAnim>().disableOutline();
                     }
-                    
+
                 }
             }
         }
@@ -62,16 +72,22 @@ public class MonsterAnim : MonoBehaviour {
         if (rotate)
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, initialrotation, Time.deltaTime * 2);
-            
+
 
             if (transform.rotation == initialrotation)
             {
-                
+
                 rotate = false;
             }
-            
+
         }
     }
+
+    void OnEnable()
+    {
+        disableOutline();
+    }
+
 
     public void getHit()
     {
@@ -80,7 +96,7 @@ public class MonsterAnim : MonoBehaviour {
 
     public void OnMouseDown()
     {
-        combatManager.monsterSelected(transform.gameObject); 
+        combatManager.monsterSelected(transform.gameObject);
     }
 
     public void attackTarget(GameObject monster)
@@ -94,6 +110,26 @@ public class MonsterAnim : MonoBehaviour {
         moving = true;
     }
 
+    public void disableOutline()
+    {
+        outliner.enabled = false;
+    }
+
+    public void enableOutline(string color)
+    {
+        outliner.enabled = true;
+
+        if (color == "green")
+        {
+            outliner.color = 1;
+        }
+        else
+        {
+            outliner.color = 0;
+        }
+
+    }
+
     IEnumerator waitForAttack()
     {
         yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
@@ -103,7 +139,8 @@ public class MonsterAnim : MonoBehaviour {
         moving = true;
     }
 
-    
-
-    
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        combatManager.monsterSelected(transform.gameObject);
+    }
 }
